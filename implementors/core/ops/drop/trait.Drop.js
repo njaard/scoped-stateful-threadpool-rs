f13b0c351bf2cdd0e0185b5b7ee97ea508063f16
Travis CI User <travis@example.org>
2017-10-21 20:02:59 +0000

    Documentation for Kimundi/scoped-threadpool-rs@5009d7d

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["scoped_threadpool"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"scoped_threadpool/struct.Pool.html\" title=\"struct scoped_threadpool::Pool\">Pool</a>","impl&lt;'pool, 'scope&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"scoped_threadpool/struct.Scope.html\" title=\"struct scoped_threadpool::Scope\">Scope</a>&lt;'pool, 'scope&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
